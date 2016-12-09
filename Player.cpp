bae599f590be5b4a850e6929fb9a450a11491908
Damian Angelone <d-angelone@hotmail.com>
2016-12-09 15:31:49 -0500

    Commented Player

--- Player.cpp
+++ Player.cpp
@@ -24,18 +24,19 @@ using namespace std;
 bool canPhysics = false;					// If the player should enter freefall
 bool moved = false;							// To add the initial spot
 bool stopped = false;						// To add the initial spot
-bool gameOver = false;
-bool currOffBlock = false;
+bool gameOver = false;						// If game is currently in game over state
+bool currOffBlock = false;					// If the player is currently off the block
 
-const int JUMPSIZE = 2;
+const int JUMPSIZE = 2;						// Jump displacement for the player
 int rot = 270;								// The rotation angle
 
 float displacement[] = {0.0, 1.5, 0.0};		// The transformation matrix
 float velocity[] = {0.0, 0.0, 0.0};			// The velocity matrix
 float acceleration[] = {0.0, 0.0, 0.0};		// The velocity matrix
 
-GLUquadric* qobj = gluNewQuadric();
+GLUquadric* qobj = gluNewQuadric();			// Used for drawing character
 
+// Parameters used to keep track of player position
 Structure::point3D Player::getCoor() {
 	Structure::point3D position;
 	position.x = displacement[0];
@@ -43,22 +44,25 @@ Structure::point3D Player::getCoor() {
 	position.z = displacement[2];
 	return position;
 }
-
+/* Keeps track if player is off the block (lose a life) */
 bool Player::currentlyOffBlock(){
 
 	return currOffBlock;
 }
 
-// Change the player's orientation
+/* Change the player's orientation */
 void Player::setRotation(int change) {
+
 	rot = change;
 }
 
+/* Stops player (touching sand/enemy) */
 void Player::setStopped(bool b) {
+	
 	stopped = b;
 }
 
-// Reset the player's position
+/* Reset the player's position */
 void Player::reset() {
 	canPhysics = false;
 	moved = false;
@@ -74,6 +78,7 @@ void Player::reset() {
 	rot = 270;
 }
 
+/* Draws the half-sphere for Q*bert's feet */
 void drawHalfSphere(int scaley, int scalex, GLfloat r) {
     glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
     glEnable(GL_COLOR_MATERIAL);
@@ -87,7 +92,6 @@ void drawHalfSphere(int scaley, int scalex, GLfloat r) {
 			v[i * scaley + j][2] = r * sin(j * 2 * M_PI/scaley) * cos(i * M_PI/(2 * scalex));
      	}
     }
- 
 	glBegin(GL_QUADS);
 		for (int i=0; i<scalex - 1; ++i) {
 	    	for (int j=0; j<scaley; ++j) {
@@ -101,6 +105,7 @@ void drawHalfSphere(int scaley, int scalex, GLfloat r) {
 	glDisable(GL_COLOR_MATERIAL);
 }
 
+/* Draws Q*bert to the scene */
 void drawQubert() {
 	glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
 	glEnable(GL_COLOR_MATERIAL);
@@ -110,15 +115,15 @@ void drawQubert() {
 		glRotatef(-270, 0, 1, 0);
 		glColor3ub(244, 67, 54);
 		glScalef(0.6, 0.6, 0.6);
-		//draw body
+		// Draw body
 		glPushMatrix();
 			glutSolidSphere(1, 100, 100);
-			//eyes
+			// Right eye
 			glPushMatrix();
 				glColor3f(1, 1, 1);
 				glTranslatef(-0.3,0.4,0.6);
 				glutSolidSphere(0.4, 100, 100);
-				//pupil
+				// Right pupil
 				glPushMatrix();
 					glColor3f(0, 0, 0);
 					glTranslatef(0, 0.1, 0.25);
@@ -126,20 +131,19 @@ void drawQubert() {
 				glPopMatrix();
 			glPopMatrix();
 		glPopMatrix();
-		
-		// other eye
+		// Left eye
 		glPushMatrix();
 			glColor3f(1, 1, 1);
 			glTranslatef(0.3, 0.4, 0.6);
 			glutSolidSphere(0.4, 100, 100);
+			// Left pupil
 			glPushMatrix();
 				glColor3f(0, 0, 0);
 				glTranslatef(0, 0.1, 0.25);
 				glutSolidSphere(0.2, 100, 100);
 			glPopMatrix();
 		glPopMatrix();
-		
-		// nose
+		// Nose
 		glDisable(GL_CULL_FACE);
 		glPushMatrix();
 			glColor3ub(244, 67, 54);
@@ -149,18 +153,17 @@ void drawQubert() {
 				glColor3ub(244, 67, 54);
 				gluDisk(qobj, 0.35, 0.55, 100, 100);
 			glPopMatrix();
-
 			glColor3f(0, 0, 0);
 			gluCylinder(qobj, 0.2, 0.35, 2, 100, 100);
 		glPopMatrix();
 		glEnable(GL_CULL_FACE);
-
-		//left foot
+		// Left leg
 		glPushMatrix();
 			glColor3ub(244, 67, 54);
 			glRotatef(90, 1, 0, 0);
 			glTranslatef(-0.3, 0, 0);
 			gluCylinder(qobj, 0.15, 0.15, 2, 100, 100);
+			// Left foot
 			glPushMatrix();
 				glColor3ub(244, 67, 54);
 				glRotatef(-90, 1, 0, 0);
@@ -169,13 +172,13 @@ void drawQubert() {
 				glutSolidCube(0.3);
 			glPopMatrix();
 		glPopMatrix();
-
-		//right foot
+		// Right leg
 		glPushMatrix();
 			glColor3ub(244, 67, 54);
 			glRotatef(90, 1, 0, 0);
 			glTranslatef(0.3, 0, 0);
 			gluCylinder(qobj, 0.15, 0.15, 2, 100, 100);
+			// Right foot
 			glPushMatrix();
 				glColor3ub(244, 67, 54);
 				glRotatef(-90, 1, 0, 0);
@@ -184,28 +187,29 @@ void drawQubert() {
 				glutSolidCube(0.3);
 			glPopMatrix();
 		glPopMatrix();
-	glPopMatrix();//body
+	glPopMatrix();
 
 	glDisable(GL_COLOR_MATERIAL);
 }
 
-// Computes the velocities from the acceleration
+/* Computes the velocities from the acceleration */
 void physics() {
 	int x, z;
 	x = floor(displacement[0] + velocity[0] + 0.5);
 	z = floor(displacement[2] + velocity[2] + 0.5);
- 	
- 	// check to see if it has touched the sand 
-	// checks if game over, or if only a single life was lost
+ 	// Checks to see if it has touched the sand 
+	// Checks if game over, or if only a single life was lost
 	if (displacement[1] + velocity[1] < -9) {
 		stopped = true;
 		Interactivity::setLives(-1);
-
+		// If the player runs out of lives
 		if(Interactivity::getLives() == 0)
-			UserInterface::setGameOverState();
+			UserInterface::setGameOverState(); // Game over
+		//Or..
 		else
-			UserInterface::setLevelState();
+			UserInterface::setLevelState(); // Level Lost
 	} 
+	// Used for when the user falls of the stage
 	else {
 		velocity[0] = acceleration[0] + velocity[0];
 		velocity[1] = acceleration[1] + velocity[1];
@@ -213,20 +217,18 @@ void physics() {
 	}
  }
 
-// Checks if the player is on a block
+/* Checks if the player is on a block */
 void offBlock() {
-
 	currOffBlock = false;
-
-	if (displacement[0] == 2 * Interactivity::getLength() ||	// far edges
+	// Checks if the suer is not on a block
+	if (displacement[0] == 2 * Interactivity::getLength() ||	// Far edges
 		displacement[2] == 2 * Interactivity::getLength() ||
-		displacement[0] < 0 || displacement[2] < 0 ||		// close edges
-		// going left/down "into" the board with no block
-		displacement[0] == displacement[2] + 2) {
-
+		displacement[0] < 0 || displacement[2] < 0 ||			// Close edges
+		displacement[0] == displacement[2] + 2) {				// going left/down "into" the board with no block
+		// Variable used for layer falling
 		currOffBlock = true; 
-
 		canPhysics = true;
+		// Player is falling
 		acceleration[1] = -0.35;
 		switch (rot) {
 			case 0:		// Left
@@ -245,13 +247,14 @@ void offBlock() {
 	}
 }
 
-// draws the player and calls the necessary logic functions
+/* Draws the player and calls the necessary logic functions */
 void Player::drawPlayer(bool step) {
+	// If the player is stopped (touched sand or enemy)
 	if (!stopped && !UserInterface::getFinishedLevelState()) {
 		if (!canPhysics)			// Check if the player is on a block
 			offBlock();
-		if (canPhysics)				// not else because offBlock changes canPhysics
-			physics();				// If the player is not on a block, do physics
+		if (canPhysics)				// We don't 'else' because offBlock changes canPhysics
+			physics();				// If the player is not on a block, run physics
 		if (Interactivity::getSpace() == 1 && !canPhysics) {	// Move the player
 			// Add the initial spot
 			if (!moved) {
@@ -259,7 +262,6 @@ void Player::drawPlayer(bool step) {
 				moved = true;
 			}
 			// Move the player based on the orientation
-			
 			switch(rot) {
 				case 0:		// Left
 					displacement[0] += JUMPSIZE;
@@ -278,7 +280,7 @@ void Player::drawPlayer(bool step) {
 					displacement[2] += JUMPSIZE;
 					break;
 			}
-			
+			// User can press 'space' again
 			Interactivity::setSpace(0);
 			// record the new position
 			Interactivity::pushPosition(displacement[0], displacement[2]);
