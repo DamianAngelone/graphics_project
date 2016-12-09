a519e019835c1f88dfdb5b58fec2d7ff5b622520
Patrick Diamond Duncan <patrick.duncan@outlook.com>
2016-12-08 22:32:33 -0500

    better looking enemy

--- Enemy.cpp
+++ Enemy.cpp
@@ -77,162 +77,177 @@ bool inBounds(int x, int z) {
 
 // Draw the model of the players
 void Enemy::draw() {
-	//glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
+	glPushAttrib(GL_LIGHTING_BIT);
+	glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
 	glEnable(GL_COLOR_MATERIAL);
 
 	float origin[3] = {0, 0, 0};
 	glPushMatrix();
-		glScalef(0.55, 0.55, 0.55);
-		glTranslatef(0, 1, 0);
-		// Rotate the body to the correct orientation
-		switch (enemyRot) {
-			case 0:
-				glRotatef(90, 0, 1, 0);
-				break;
-			case 1:
-				glRotatef(180, 0, 1, 0);
-				break;
-			case 2:
-				glRotatef(270, 0, 1, 0);
-				break;
-			case 3:
-				glRotatef(0, 0, 1, 0);
-				break;
-		}
+	glScalef(0.55, 0.55, 0.55);
+	glTranslatef(0, 1, 0);
+	// Rotate the body to the correct orientation
+	switch (enemyRot) {
+		case 0:
+			glRotatef(90, 0, 1, 0);
+			break;
+		case 1:
+			glRotatef(180, 0, 1, 0);
+			break;
+		case 2:
+			glRotatef(270, 0, 1, 0);
+			break;
+		case 3:
+			glRotatef(0, 0, 1, 0);
+			break;
+	}
+	float amb[3];
+	float diff[3];
+	float spec[3];
+	amb[0] = 0.05375;
+	amb[1] = 0.05;
+	amb[2] = 0.06625;
+	amb[3] = 1;
+	diff[0] = 0.18275;
+	diff[1] = 0.17;
+	diff[2] = 0.22525;
+	diff[3] = 1;
+	spec[0] = 0.332741;
+	spec[1] = 0.328634;
+	spec[2] = 0.346435;
+	spec[3] = 1;
+	glMaterialfv(GL_FRONT, GL_AMBIENT, amb);
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, diff);
+	glMaterialfv(GL_FRONT, GL_SPECULAR, spec);
+	glMaterialf(GL_FRONT, GL_SHININESS, 0.3);
 
-		//draw body
-		glColor3f(0.5,0.2,1);
-		glutSolidSphere(1,100,100);
+	//draw body
+	glColor3ub(0, 0, 0);
+	glutSolidSphere(1,100,100);
+		glPushMatrix();
+			glColor3f(1, 1, 1);
+			glTranslatef(-0.3,0.4,0.6);
+			glutSolidSphere(0.4, 100, 100);
+			//pupil
 			glPushMatrix();
-				glColor3f(1, 1, 1);
-				glTranslatef(-0.3,0.4,0.6);
-				glutSolidSphere(0.4, 100, 100);
-				//pupil
-				glPushMatrix();
-					glColor3f(0, 0, 0);
-					glTranslatef(0, 0.1, 0.25);
-					glutSolidSphere(0.2, 100, 100);
-				glPopMatrix();
-			glPopMatrix();
-			// other eye
-			glPushMatrix();
-				glColor3f(1, 1, 1);
-				glTranslatef(0.3, 0.4, 0.6);
-				glutSolidSphere(0.4, 100, 100);
-				glPushMatrix();
-					glColor3f(0, 0, 0);
-					glTranslatef(0, 0.1, 0.25);
-					glutSolidSphere(0.2, 100, 100);
-				glPopMatrix();
-			glPopMatrix();
-			
-			// EAR1
-			glDisable(GL_CULL_FACE);
-			glPushMatrix();
-				glRotatef(-90,0,1,0);
-				glRotatef(-45,1,0,0);
-				glColor3f(0.5, 0.2, 1);
-				gluCylinder(qObj, 0.35, 0.55, 2, 100, 100);
-				glPushMatrix(); //disc
-					glTranslatef(0, 0, 2);
-					glColor3f(0.5, 0.2, 1);
-					gluDisk(qObj, 0.35, 0.55, 100, 100);
-				glPopMatrix();
-
 				glColor3f(0, 0, 0);
-				gluCylinder(qObj, 0.2, 0.35, 2, 100, 100);
-
+				glTranslatef(0, 0.1, 0.25);
+				glutSolidSphere(0.2, 100, 100);
 			glPopMatrix();
-			glEnable(GL_CULL_FACE);
-
-			// EAR2
-			glDisable(GL_CULL_FACE);
+		glPopMatrix();
+		// other eye
+		glPushMatrix();
+			glColor3f(1, 1, 1);
+			glTranslatef(0.3, 0.4, 0.6);
+			glutSolidSphere(0.4, 100, 100);
 			glPushMatrix();
-				glRotatef(90,0,1,0);
-				glRotatef(-45,1,0,0);
-				glColor3f(0.5, 0.2, 1);
-				gluCylinder(qObj, 0.35, 0.55, 2, 100, 100);
-				glPushMatrix(); //disc
-					glTranslatef(0, 0, 2);
-					glColor3f(0.5, 0.2, 1);
-					gluDisk(qObj, 0.35, 0.55, 100, 100);
-				glPopMatrix();
-
 				glColor3f(0, 0, 0);
-				gluCylinder(qObj, 0.2, 0.35, 2, 100, 100);
-
+				glTranslatef(0, 0.1, 0.25);
+				glutSolidSphere(0.2, 100, 100);
 			glPopMatrix();
-			glEnable(GL_CULL_FACE);
+		glPopMatrix();
+		
+		// EAR1
+		glDisable(GL_CULL_FACE);
+		glPushMatrix();
+			glScalef(0.5, 1, 1);
+			glRotatef(-90,0,1,0);
+			glRotatef(-45,1,0,0);
+			glColor3ub(120, 120, 120);		// grey
+			gluCylinder(qObj, 0.35, 0.55, 2, 100, 100);
+			glPushMatrix(); //disc
+				glTranslatef(0, 0, 2);
+				gluDisk(qObj, 0.35, 0.55, 100, 100);
+			glPopMatrix();
 
-			//left foot front
+			glColor3f(0, 0, 0);
+			gluCylinder(qObj, 0.2, 0.35, 2, 100, 100);
+
+		glPopMatrix();
+		glEnable(GL_CULL_FACE);
+
+		// EAR2
+		glDisable(GL_CULL_FACE);
+		glPushMatrix();
+			glScalef(0.5, 1, 1);
+			glRotatef(90,0,1,0);
+			glRotatef(-45,1,0,0);
+			glColor3ub(120, 120, 120);		// grey
+			gluCylinder(qObj, 0.35, 0.55, 2, 100, 100);
+			glPushMatrix(); //disc
+				glTranslatef(0, 0, 2);
+				gluDisk(qObj, 0.35, 0.55, 100, 100);
+			glPopMatrix();
+
+			glColor3f(0, 0, 0);
+			gluCylinder(qObj, 0.2, 0.35, 2, 100, 100);
+
+		glPopMatrix();
+		glEnable(GL_CULL_FACE);
+
+		//left foot front
+		glPushMatrix();
+			glColor3ub(120, 120, 120);		// grey
+			glRotatef(70, 1, 0, 0);
+			glRotatef(-20,0,1,0);
+			glTranslatef(-0.3, 0, 0);
+			gluCylinder(qObj, 0.20, 0.4, 2, 100, 100);
 			glPushMatrix();
-				glColor3f(0.5, 0.2, 1);
-				glRotatef(70, 1, 0, 0);
-				glRotatef(-20,0,1,0);
-				glTranslatef(-0.3, 0, 0);
-				gluCylinder(qObj, 0.20, 0.4, 2, 100, 100);
-				glPushMatrix();
-				
-					glRotatef(-90, 1, 0, 0);
-
-					glTranslatef(0, -2.0, 0.1);
-					glutSolidSphere(0.5,100,100);
-				glPopMatrix();
+				glRotatef(-90, 1, 0, 0);
+				glColor3ub(0, 0, 0);
+				glTranslatef(0, -2.0, 0.1);
+				glutSolidSphere(0.5,100,100);
 			glPopMatrix();
+		glPopMatrix();
 
-			//left foot back
+		//left foot back
+		glPushMatrix();
+			glColor3ub(120, 120, 120);		// grey
+			glRotatef(180,0,1,0);
+			glRotatef(70, 1, 0, 0);
+			glRotatef(-20,0,1,0);
+			glTranslatef(-0.3, 0, 0);
+			gluCylinder(qObj, 0.20, 0.4, 2, 100, 100);
 			glPushMatrix();
-				glColor3f(0.5, 0.2, 1);
-				glRotatef(180,0,1,0);
-				glRotatef(70, 1, 0, 0);
-				glRotatef(-20,0,1,0);
-				glTranslatef(-0.3, 0, 0);
-				gluCylinder(qObj, 0.20, 0.4, 2, 100, 100);
-				glPushMatrix();
-				
-					glRotatef(-90, 1, 0, 0);
-
-					glTranslatef(0, -2.0, 0.1);
-					glutSolidSphere(0.5,100,100);
-				glPopMatrix();
+				glRotatef(-90, 1, 0, 0);
+				glColor3ub(0, 0, 0);
+				glTranslatef(0, -2.0, 0.1);
+				glutSolidSphere(0.5,100,100);
 			glPopMatrix();
+		glPopMatrix();
 
-			//right foot front
+		//right foot front
+		glPushMatrix();
+			glColor3ub(120, 120, 120);		// grey
+			glRotatef(70, 1, 0, 0);
+			glRotatef(20,0,1,0);
+			glTranslatef(0.3, 0, 0);
+			gluCylinder(qObj, 0.20, 0.4, 2, 100, 100);
 			glPushMatrix();
-				glColor3f(0.5, 0.2, 1);
-				glRotatef(70, 1, 0, 0);
-				glRotatef(20,0,1,0);
-				glTranslatef(0.3, 0, 0);
-				gluCylinder(qObj, 0.20, 0.4, 2, 100, 100);
-				glPushMatrix();
-				
-					glRotatef(-90, 1, 0, 0);
-
-					glTranslatef(0, -2.0, 0.1);
-					glutSolidSphere(0.5,100,100);
-				glPopMatrix();
+				glRotatef(-90, 1, 0, 0);
+				glColor3ub(0, 0, 0);
+				glTranslatef(0, -2.0, 0.1);
+				glutSolidSphere(0.5,100,100);
 			glPopMatrix();
+		glPopMatrix();
 
-			//right foot back
+		//right foot back
+		glPushMatrix();
+			glColor3ub(120, 120, 120);		// grey
+			glRotatef(180,0,1,0);
+			glRotatef(70, 1, 0, 0);
+			glRotatef(20,0,1,0);
+			glTranslatef(0.3, 0, 0);
+			gluCylinder(qObj, 0.20, 0.4, 2, 100, 100);
 			glPushMatrix();
-				glColor3f(0.5, 0.2, 1);
-				glRotatef(180,0,1,0);
-				glRotatef(70, 1, 0, 0);
-				glRotatef(20,0,1,0);
-				glTranslatef(0.3, 0, 0);
-				gluCylinder(qObj, 0.20, 0.4, 2, 100, 100);
-				glPushMatrix();
-				
-					glRotatef(-90, 1, 0, 0);
-
-					glTranslatef(0, -2.0, 0.1);
-					glutSolidSphere(0.5,100,100);
-				glPopMatrix();
+				glRotatef(-90, 1, 0, 0);
+				glColor3ub(0, 0, 0);
+				glTranslatef(0, -2.0, 0.1);
+				glutSolidSphere(0.5,100,100);
 			glPopMatrix();
-
+		glPopMatrix();
 	glPopMatrix();
 	glDisable(GL_COLOR_MATERIAL);
-
+	glPopAttrib();
 }
 
 // draws the player and calls the necessary logic functions
